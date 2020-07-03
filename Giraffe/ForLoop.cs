7cd75ffd29dcca2e0abe3df3f265ae4d92bec62b
josee.n.paquette <josee.n.paquette@hrsdc-rhdcc.gc.ca>
2020-07-03 16:31:34 -0400

    Still wip, need to fix GetLuckyNumbers

---
+++ Giraffe/ForLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    public class ForLoop
+    {
+        public static int[] GetLuckyNumbers(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                //int[] arrayNumbers;
+                    Console.WriteLine(array[i]);
+                //return array[i];
+                //arrayNumbers = Console.WriteLine(array[i]);
+
+                //int[] arrayNumbers = new array[i];
+
+                //return arrayNumbers;
+            }
+            
+        }
+    }
+}
