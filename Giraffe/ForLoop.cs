a68dcd00f90284a274269766c5e1f2c4c6b4f27e
josee.n.paquette <josee.n.paquette@hrsdc-rhdcc.gc.ca>
2020-07-10 16:20:23 -0400

    still wip, fix GetLuckyNumbers

--- Giraffe/ForLoop.cs
+++ Giraffe/ForLoop.cs
@@ -8,20 +8,38 @@ namespace Giraffe
 {
     public class ForLoop
     {
+        /// <summary>
+        /// Example of a for loop that returns each item of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
         public static int[] GetLuckyNumbers(int[] array)
         {
+            //int[] arrayNumbers;
+
             for (int i = 0; i < array.Length; i++)
             {
-                //int[] arrayNumbers;
-                    Console.WriteLine(array[i]);
+                int[] array = i;
+                
+                //arrayNumbers = new int[] { i };
+
+                //int[] arrayNumbers = array;
+
+                //int[] arrayNumbers = new int[] { array.Length };
+                //arrayNumbers = new int[] { array[i] };
+
+                Console.WriteLine(arrayNumbers);
+                //int[] arrayNumbers = null;
+                //Console.WriteLine(array[i]);
                 //return array[i];
                 //arrayNumbers = Console.WriteLine(array[i]);
 
-                //int[] arrayNumbers = new array[i];
+                //int[] arrayNumbers = array[i];
 
-                //return arrayNumbers;
+                //int[] arrayNumbers = null;
+                
             }
-            
+            return array;
         }
     }
 }
